d27b6b6245b4f7124d9378d4ddeda194c3c9ce54
richard tsang <richard.tsang9641@gmail.com>
2025-05-20 19:59:35 +0800

    add dashboard page

---
+++ app/(overview)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <h1 data-testid="page:dashboard" className='font-bold text-2xl'>Dashboard</h1>
+        </div>
+    )
+}
+
+export default page
