2e9d1006a29886b305582264dc85c0cb24b3c0cd
richard tsang <richard.tsang9641@gmail.com>
2025-05-20 22:10:08 +0800

    add FoodPriceTable to page - dashboard

--- app/(overview)/dashboard/page.tsx
+++ app/(overview)/dashboard/page.tsx
@@ -1,9 +1,11 @@
+import FoodPriceTable from '@/features/foodPrice/component/FoodPriceTable'
 import React from 'react'
 
 const page = () => {
     return (
-        <div>
-            <h1 data-testid="page:dashboard" className='font-bold text-2xl'>Dashboard</h1>
+        <div className='flex flex-col gap-6'>
+            <h1 data-testid="page:dashboard" className='font-bold text-2xl'>Dashboard dasd</h1>
+            <FoodPriceTable/>
         </div>
     )
 }
