54927b818e13658e7c80e1b70d0b819a91a0b983
richard tsang <richard.tsang9641@gmail.com>
2025-05-21 18:16:40 +0800

    optimize component - button

--- components/common/MuiButton.tsx
+++ components/common/MuiButton.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react'
 import { Button, ButtonOwnProps } from '@mui/material'
 const MuiButton = ({ onClick, children, ...props }: ButtonOwnProps & React.ButtonHTMLAttributes<HTMLButtonElement>) => {
   return (
-    <Button onClick={onClick} variant='contained' color='primary' {...props} className='rounded-md font-semibold capitalize'>{children}</Button>
+    <Button onClick={onClick} variant='contained' color='primary' {...props} className='rounded-md font-semibold capitalize text-white'>{children}</Button>
   )
 }
 
